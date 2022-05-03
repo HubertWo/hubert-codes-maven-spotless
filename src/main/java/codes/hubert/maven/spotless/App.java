336c4fd04252057795106e40a7854a3582845d48
Hubert Wo <2518652+HubertWo@users.noreply.github.com>
2022-05-03 18:11:45 +0200

    Spotless - Java 17, example code

---
+++ src/main/java/codes/hubert/maven/spotless/App.java
@@ -0,0 +1,22 @@
+package codes.hubert.maven.spotless;
+
+/**
+ * Messy
+ *          code
+ *          makes
+ *           programmers
+ * life
+ *  harder.</p>
+ */
+public class App {
+
+  final static public String myConst = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore ";
+
+  public static void main(String[] args) {
+    System.out
+            .println(myConst);
+    System
+            .out
+            .println("Please fix me :)");
+  }
+}
